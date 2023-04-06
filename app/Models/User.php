7a3d98a99cf8971f021680d22a9b67fa942d0171
talel <talelmejri8@gmail.com>
2023-04-06 10:20:14 +0100

    add association

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,10 @@ class User extends Authenticatable
         return $this->hasMany(EvenementSportif::class);
     }
 
+    public function roles(){
+        return $this->belongsToMany(Role::class,'role_user','user_id','role_id')->withTimestamps();
+    }
+
     /**
      * The attributes that are mass assignable.
      *
