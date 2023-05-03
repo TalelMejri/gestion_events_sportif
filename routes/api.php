6c037a0b106404505f5cdb2752380bd6043022d9
talel <talelmejri8@gmail.com>
2023-05-03 18:56:19 +0100

    add api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HumansController;
@@ -18,5 +19,7 @@ use App\Http\Controllers\HumansController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
 
 
