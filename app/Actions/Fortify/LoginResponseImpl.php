12393acadb7c84d3617cba0855fab0652fb8e5a2
talel <talelmejri8@gmail.com>
2023-05-13 12:28:19 +0100

    add deux classe implments from interface  fortify

---
+++ app/Actions/Fortify/LoginResponseImpl.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use Laravel\Fortify\Contracts\LoginResponse;
+class LoginResponseImpl implements LoginResponse
+{
+
+    public function toResponse($request)
+    {
+
+        if($request->user()->role=='Organisateur'){
+            return redirect()->route('eventSportifs.index');
+        }
+        elseif ($request->user()->role=='Admin'){
+            return redirect()->route('admin.events');
+        }
+        else{
+            return redirect()->route('visitor');
+        }
+    }
+}
+
+?>
---
+++ app/Actions/Fortify/RegisterResponseImpl.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use Laravel\Fortify\Contracts\RegisterResponse;
+
+class RegisterResponseImpl implements RegisterResponse
+{
+
+    public function toResponse($request)
+    {
+        if($request['role']=='Organisateur'){
+            return redirect()->route('eventSportifs.index');
+        }
+        elseif ($request['role']=='Admin'){
+            return redirect()->route('admin');
+        }
+        else{
+            return redirect()->route('visitor');
+        }
+
+    }
+}
