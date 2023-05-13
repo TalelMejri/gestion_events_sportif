ac4b4d46a50c77fc9b2e8fe3438c851fc0643270
talel <talelmejri8@gmail.com>
2023-05-13 12:26:32 +0100

    add views

---
+++ resources/views/admin/event-dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main')
+@section('content')
+    <h2>Admin: Events Dashboard</h2>
+@endsection
---
+++ resources/views/admin/user-dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.main')
+@section('content')
+    <h2>Admin: Users Dashboard</h2>
+@endsection
