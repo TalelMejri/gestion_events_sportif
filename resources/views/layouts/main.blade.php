85823a4d7d9c412fc0009f174ea3ff705596d70c
talel <talelmejri8@gmail.com>
2023-05-13 12:26:09 +0100

    change main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,45 +4,94 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="{{$description ?? ''}}">
-    <title>{{$titel}}</title>
-    @vite(['resources/scss/app.scss','resources/js/app.js'])
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="description" content="{{ $description ?? '' }}">
+    <title>{{$title ?? 'Event Sportif'}}</title>
+
+    <!-- Scripts and CSS-->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-   <div class="container-fluid mh-100" style="height: 100vh">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="{{route('home')}}">{{$heading}}</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Events</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Categorie</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <div class="container-fluid mh-100" style="height: 100vh">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{route('home')}}">{{$heading ?? 'Event Sportif'}}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Events</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <div style="min-height: 50vh">
             <main>
-                 @yield('content')
+                @yield('content')
+
             </main>
+
         </div>
 
-        <div>
-             <footer class="d-flex  justify-content-center align-items-center py-3 my-4 border-top">
-                <p class="col-md-4 mb-0 text-center">&copy;2023 ISET BIZERTE DSI22</p>
-              </footer>
-        </div>
+        <footer class="footer mt-auto py-3 bg-light">
+            <div class="container">
+                <p class="text-center">
+                    <span class="text-muted text-center">Copyright &copy; 2023 ISET Bizerte DSI22</span>
+                </p>
+
+
+            </div>
+        </footer>
     </div>
+
 </body>
 </html>
