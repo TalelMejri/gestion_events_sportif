9e7f579dec56ae0687c8a8e3fbed71c33a7e16e3
talel <talelmejri8@gmail.com>
2023-05-13 12:22:01 +0100

    add champ role in user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['Admin','Organisateur','Visitor']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
